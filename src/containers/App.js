b6215bc89703017f741e6955117b3ec9da3f3bb1
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-03-31 20:38:15 +0430

    feat: added custom scrollbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,14 @@
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
+
 import Main from '../components/Main';
 import Sidebar from '../components/Sidebar';
 
 export default function App() {
   return (
-    <>
+    <SimpleBar style={{ maxHeight: '100vh' }}>
       <Main />
       <Sidebar />
-    </>
+    </SimpleBar>
   );
 }
