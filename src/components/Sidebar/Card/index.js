c8b216ef2c4cc9bc9c4d779360875c15b52724c1
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-04-01 00:02:23 +0430

    feat: added image zoom

--- src/components/Sidebar/Card/index.js
+++ src/components/Sidebar/Card/index.js
@@ -1,9 +1,14 @@
+import ImageZoom from 'react-medium-image-zoom';
+
 import './index.css';
 
 const Card = ({ name, title, image }) => {
   return (
     <li className="card">
-      <img src={`/img/${image}`} alt={name} />
+      <ImageZoom
+        image={{ src: `/img/${image}`, alt: name }}
+        zoomImage={{ src: `/img/${image}`, alt: name }}
+      />
       <div>
         <h4>{name}</h4>
         <p>{title}</p>
