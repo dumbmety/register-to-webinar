b319f21401b2870d47bb65399ae35c553c5bef10
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-04-01 00:14:36 +0430

    feat: added react helmet

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,3 +1,4 @@
+import Helmet from 'react-helmet';
 import SimpleBar from 'simplebar-react';
 import 'simplebar/dist/simplebar.min.css';
 
@@ -6,12 +7,28 @@ import Sidebar from '../components/Sidebar';
 
 const App = () => {
   return (
-    <SimpleBar style={{ maxHeight: '100vh' }}>
-      <div className="layout">
-        <Main />
-        <Sidebar />
-      </div>
-    </SimpleBar>
+    <>
+      <Helmet>
+        <title>Register to Webinar</title>
+        <meta property="og:title" content="The help of Augmented Reality and Holograms." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://register-to-webinar.netlify.app" />
+        <meta
+          property="og:image"
+          content="https://register-to-webinar.netlify.app/img/screenshot.png"
+        />
+        <meta
+          property="og:description"
+          content="Use Augmented Reality to keep students engaged and provide detailed explanations of models and course material. In a 3D, AR Lab, students can pinch, zoom, and rotate equipment related to the course, for a fully immersive learning experienc"
+        />
+      </Helmet>
+      <SimpleBar style={{ maxHeight: '100vh' }}>
+        <div className="layout">
+          <Main />
+          <Sidebar />
+        </div>
+      </SimpleBar>
+    </>
   );
 };
 
