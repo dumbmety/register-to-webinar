74f2a9365ddcc2bcaa0ac428d4bdb986e7e4b50c
Mehdi Neysi <dev.mehdineysi@gmail.com>
2022-04-10 00:51:56 +0430

    refactor: add vazir from google fonts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom';
-import App from './containers/App';
+import ReactDOM from "react-dom"
+import App from "./containers/App"
 
-import 'simplebar/dist/simplebar.min.css';
-import './index.css';
+import "simplebar/dist/simplebar.min.css"
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"))
